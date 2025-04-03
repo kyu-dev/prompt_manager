50293a7c56e37fb54130fdd00afbc3526326611c
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-04-03 10:10:00 +0200

    fix redirection à la première connexion

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -2,7 +2,9 @@ import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }) {
   const user = localStorage.getItem('user');
-  if (!user) {
+
+  if (!user || user === 'undefined' || user === 'null') {
+    // on S'assure que il n'y a rien dans le localstorage car au lancement localstorage est sur undefine ce qui est condirer user = true
     return <Navigate to="/login" replace />;
   }
   return children;
