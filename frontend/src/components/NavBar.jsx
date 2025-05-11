eef62c962b0a8871b3ed2c0d8109650b98c25182
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-11 11:26:25 +0200

    mise en place de shadcn ui

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { House } from 'lucide-react';
+import { Button } from './ui/button';
 
 const NavBar = () => {
   return (
     <nav className="bg-amber-400">
       <Link to="/" className="flex flex-col">
         <House />
-        Home
+        <Button />
       </Link>
     </nav>
   );
