455952a8382fb3cdedce0459e0039d0ff61a4fe3
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-11 15:51:27 +0200

    Ajout de la gestion des raccourcis clavier pour basculer la sidebar et amélioration du style du composant SideBar.

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,29 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { House, PanelLeftClose, PanelLeftOpen } from 'lucide-react';
+import { House, PanelLeftClose, PanelLeftOpen, CirclePlus } from 'lucide-react';
 
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState(true);
 
   const toggleSidebar = () => {
-    setIsOpen(!isOpen);
+    setIsOpen((prev) => !prev); //switch l'état de isOpen de true à false et de false à true
   };
 
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if ((e.metaKey || e.ctrlKey) && (e.key === 'k' || e.key === 'K')) {
+        //cmd+k pour ouvrir la sidebar
+        toggleSidebar();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown); //clean l'event listener
+    };
+  }, []);
+
   return (
-    <div className="flex">
-      <nav className={`bg-amber-400 h-screen ${isOpen ? 'w-64' : 'w-0'}`}>
-        <div className="flex justify-between items-center p-4">
-          {isOpen && (
-            <Link to="/" className="flex gap-2">
-              <House size={20} />
-              <span>Dashboard</span>
-            </Link>
-          )}
+    <div className="flex h-screen">
+      <nav
+        className={`bg-blue-950 ${
+          isOpen ? 'w-64' : 'w-16' // si isOpen est true agrandie la w de la sidebar
+        } flex flex-col  justify-between`}
+      >
+        <div className="flex flex-col p-4 text-white gap-5">
+          <Link to="/" className="flex items-center gap-2 ">
+            <House size={20} />
+            {isOpen && <span>Home</span>}
+          </Link>
+
+          <div className="flex items-center gap-2 ">
+            <CirclePlus size={20} />
+            {isOpen && <span>Créer un prompt</span>}
+          </div>
+        </div>
+
+        <div className="p-4 mt-auto">
+          <button
+            onClick={toggleSidebar}
+            className="focus:outline-none text-white"
+            aria-label="Toggle Sidebar"
+          >
+            {isOpen ? (
+              <PanelLeftClose size={20} />
+            ) : (
+              <PanelLeftOpen size={20} />
+            )}
+          </button>
         </div>
       </nav>
-      <button onClick={toggleSidebar} className="p-2 ">
-        {isOpen ? <PanelLeftClose size={20} /> : <PanelLeftOpen size={20} />}
-      </button>
     </div>
   );
 };
