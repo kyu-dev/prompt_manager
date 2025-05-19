df4405071fd68085d259e54bcdf7f5d16e30014e
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-19 15:39:46 +0200

    Ajout de nouvelles icônes pour les favoris et mise à jour de la page d'accueil avec une section dédiée aux favoris. Amélioration de la mise en page pour un affichage plus fluide des cartes.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card } from '@/components/ui/card';
 import { ClipboardCheckIcon } from 'lucide-react';
 import { Clock } from 'lucide-react';
+import { Star } from 'lucide-react';
+import { Folder } from 'lucide-react';
 
 const Home = () => {
   // Exemple de données pour les cartes
@@ -15,8 +17,23 @@ const Home = () => {
   return (
     <div className="container flex flex-col mx-auto px-8 pt-8 w-screen gap-5">
       <h1 className="text-6xl font-bold mb-20">Hello Arthur 👋</h1>
+
       <h2 className="text-2xl flex items-center gap-2">
         Historique des promps <Clock />
+      </h2>
+      <div className="flex gap-6 overflow-x-auto  py-2">
+        {cardsData.map((card, index) => (
+          <Card key={index} className="w-[300px] h-[100px] p-2 flex-shrink-0">
+            <h3 className="text-xl">{card.title}</h3>
+            <button>
+              <ClipboardCheckIcon className="hover:cursor-pointer" />
+            </button>
+          </Card>
+        ))}
+      </div>
+
+      <h2 className="text-2xl flex items-center gap-2">
+        Favories <Star />
       </h2>
       <div className="flex gap-6 overflow-x-auto  py-2">
         {cardsData.map((card, index) => (
