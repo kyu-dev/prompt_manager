ad87d71b1b3b1973ee9b16f3a07ca6d72dcab25d
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-24 11:43:35 +0200

    Ajout du composant ProtectedRoute pour sécuriser l'accès aux pages Home et About. Mise à jour des routes dans appRouter.jsx pour intégrer cette protection.

--- frontend/src/appRouter.jsx
+++ frontend/src/appRouter.jsx
@@ -3,14 +3,29 @@ import Home from './pages/Home';
 import About from './pages/About';
 import LoginPage from './auth/LoginPage';
 import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoutes';
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <ProtectedRoute>
+                <About />
+              </ProtectedRoute>
+            }
+          />
         </Route>
         <Route path="/login" element={<LoginPage />} />
       </Routes>
