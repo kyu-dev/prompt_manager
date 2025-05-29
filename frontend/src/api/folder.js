9d98fefd4369bcaf7534ffaf1f68da3ba788ece1
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-29 12:08:56 +0200

    Ajout de la fonction createFolder dans le fichier folder.js pour permettre la création de dossiers via une requête API. Gestion des erreurs incluse pour assurer la robustesse de la connexion.

--- frontend/src/api/folder.js
+++ frontend/src/api/folder.js
@@ -14,3 +14,22 @@ export async function getFolder() {
     console.error('Erreur lors de la connexion:', error);
   }
 }
+
+export async function createFolder(title) {
+  try {
+    const res = await fetch('http://localhost:3000/folder/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        title: title,
+      }),
+    });
+    const json = res.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
