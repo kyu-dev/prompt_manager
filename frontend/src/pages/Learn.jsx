eb7b219574a39d5a15541239d2113ba9989d0e7c
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-05 14:04:32 +0200

    Ajout de la dépendance "remark-gfm" dans pnpm-lock.yaml pour le rendu Markdown. Suppression de la configuration Tailwind CSS dans tailwind.config.js. Mise à jour du fichier index.css pour intégrer le plugin "@tailwindcss/typography". Refactorisation du composant Learn pour simplifier la gestion des erreurs et améliorer le rendu du contenu Markdown.

--- frontend/src/pages/Learn.jsx
+++ frontend/src/pages/Learn.jsx
@@ -1,35 +1,18 @@
-import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { useEffect, useState } from 'react';
 
 const Learn = () => {
-  const [markdown, setMarkdown] = useState(null);
-  const [error, setError] = useState(null);
+  const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
     fetch('/blog/learn.md')
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Le fichier Markdown n'a pas pu être chargé.");
-        }
-        return res.text();
-      })
+      .then((res) => res.text())
       .then(setMarkdown)
-      .catch((err) => {
-        console.error(err);
-        setError(err.message);
-      });
+      .catch(console.error);
   }, []);
 
-  if (error) {
-    return <div className="prose mx-auto p-4 text-red-500">{error}</div>;
-  }
-
-  if (!markdown) {
-    return <div className="prose mx-auto p-4">Chargement en cours...</div>;
-  }
-
   return (
-    <div className="prose mx-auto p-4">
+    <div className="prose">
       <ReactMarkdown>{markdown}</ReactMarkdown>
     </div>
   );
