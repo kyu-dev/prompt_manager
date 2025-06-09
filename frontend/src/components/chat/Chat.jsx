d46a729ce323b255049df3f7c0d8499e6b473c4e
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-06-09 12:01:25 +0200

    Ajout d'un nouveau fichier gemini.js pour intégrer l'API GoogleGenAI, permettant de générer du contenu AI. Mise à jour du composant Chat pour simplifier la structure en retirant une classe CSS superflue.

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -4,7 +4,7 @@ import ChatSection from './ChatSection';
 
 const Chat = () => {
   return (
-    <div className="bg-blue-200 w-200 h-200 border-2 border-gray-900 rounded-2xl p-10">
+    <div className=" w-200 h-200 border-2 border-gray-900 rounded-2xl p-10">
       <ChatSection />
       <ChatBar />
     </div>
